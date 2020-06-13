0ebd9136319c8ecf10d4a1b344700da6d20a973d
tathianam <tathianam@gmail.com>
2020-06-13 18:46:01 -0300

    comentarios sobre o use strict

--- 01 - variaveis/05-escopo-bloco-let.js
+++ 01 - variaveis/05-escopo-bloco-let.js
@@ -1,4 +1,6 @@
 'use strict'
+// https://pt.stackoverflow.com/questions/2746/quando-por-que-e-como-utilizar-a-diretiva-use-strict-em-javascript
+// Essa linha acima serve para nao deixar usar variavel global sem var, sem let, sem const (a festa da uva basicamente...)
 
 // ## Escopo de bloco
 
@@ -9,5 +11,5 @@
     console.log(teste);
     // Teste
 }
-teste = 'Teste 2' // Erro
+teste = 'Teste 2' // Erro (esse erro nao daria se nao tivese a linha do strict la em cima)
 console.log(teste);
